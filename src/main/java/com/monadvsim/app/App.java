46a5dd1c19df6774fec41aeaab801a0c2841b62e
salvador <atagong5@gmail.com>
2026-01-23 03:26:37 +0300

    code rebased

--- src/main/java/com/monadvsim/app/App.java
+++ src/main/java/com/monadvsim/app/App.java
@@ -1,16 +1,9 @@
 package com.monadvsim.app;
 
-import com.monadvsim.app.models.Project;
-import com.monadvsim.app.views.MainWindow;
-import com.monadvsim.app.controllers.MainController;
 
-/**
- * Entry point
- */
+
 public class App {
     public static void main(String[] args) {
-        Project project = new Project();
-        MainWindow view = new MainWindow();
-        MainController controller = new MainController(project, view);
+        System.out.print("Hello world");
     }
 }
